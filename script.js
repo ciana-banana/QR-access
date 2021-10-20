c4a2d96aefb9038fa8a994a670d3afdd9637ceb5
ciana-banana <52759619+ciana-banana@users.noreply.github.com>
2021-10-20 13:22:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+  }
+  
+  function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
+  }
+  
+  let html5QrcodeScanner = new Html5QrcodeScanner(
+      "qr-reader", { fps: 10, qrbox: 250 }, /* verbose= */ false);
+  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
